94f0165f584b94649733320251a63a3242666bd3
CampingAvocado <msaladin@proton.me>
2025-01-27 16:35:37 +0100

    Adding vcpkg integration
    new option for using vcpkg
    currently only for clfft and fftw. No runtime tests yet.
    fftwq is not included in vcpkg port.
    other CPU back-ends require fftw wrapper, not sure on how to add that
    yet.

--- cmake/gearshifft_options.cmake
+++ cmake/gearshifft_options.cmake
@@ -6,6 +6,7 @@ option(GEARSHIFFT_VERBOSE "Verbose output during build generation." OFF)
 option(GEARSHIFFT_USE_STATIC_LIBS "Force static linking Boost and FFTW (use libraries' cmake variables otherwise)." OFF)
 option(GEARSHIFFT_TESTS_ADD_CPU_ONLY "Only add tests which run on CPU." OFF)
 option(GEARSHIFFT_FLUSH_CACHE "Attempt to flush the cache before each plan and execution step. (Experimental)" OFF)
+option(GEARSHIFFT_USE_VCPKG "Try to get back-ends through vcpkg where applicable." OFF)
 
 set(GEARSHIFFT_CXX11_ABI "1" CACHE STRING "Enable _GLIBCXX_USE_CXX11_ABI in GCC 5.0+")
 set_property(CACHE GEARSHIFFT_CXX11_ABI PROPERTY STRINGS "0;1")
